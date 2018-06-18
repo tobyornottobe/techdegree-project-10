c014c2dbe9a0cb61d6ca950e6e57c0c52c31bee7
tobyornottobe <straurum@gmx.ch>
2018-06-18 23:35:36 +0200

    modal IV

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
-let employees = [];
+
 let currentTarget = '';
 
 
@@ -11,42 +11,47 @@ let currentTarget = '';
     dataType: 'json',
     success: function(data) {
       console.log(data);
-      let dr = data.results;
+      let employee = data.results;
+
 
       var employeeHTML = '';
-      if (data.results.length > 0) {
+      if (employee.length > 0) {
+
 
         //$.each(array, function(index,value) {};)
-        $.each(data.results,function(i, dr) {
+        $.each(employee, function(i, employee) {
 
-          employeeHTML += '<a class="employee-card-a" href="#employees'+i+'" data-caption="'+dr.name.first+dr.name.last+'" rel="modal:open">'
+          employeeHTML += '<a class="employee-card-a" href="#employees'+i+'" data-caption="'+employee.name.first+employee.name.last+'" rel="modal:open">'
           employeeHTML += '<div class="employee-card">'
           employeeHTML +=   '<div class="employee-card-left">'
-          employeeHTML +=     '<img src="'+dr.picture.large+'" alt="'+dr.name.first+'">'
+          employeeHTML +=     '<img src="'+employee.picture.large+'" alt="'+employee.name.first+'">'
           employeeHTML +=   '</div>'
           employeeHTML +=   '<div class="employee-card-right">'
-          employeeHTML +=     '<h2>'+dr.name.first+' '+dr.name.last+'</h2>'
-          employeeHTML +=     '<p class="lowercase">'+dr.email+'</p>'
-          employeeHTML +=     '<p>'+dr.location.city+'</p>'
+          employeeHTML +=     '<h2>'+employee.name.first+' '+employee.name.last+'</h2>'
+          employeeHTML +=     '<p class="lowercase">'+employee.email+'</p>'
+          employeeHTML +=     '<p>'+employee.location.city+'</p>'
           employeeHTML +=   '</div>'
           employeeHTML += '</div></a>'
           employeeHTML += '<div id="employees'+i+'" class="modal">'
 
-
-         modal(employees[i], i);
+            modal(employee[i], i);
 
           employeeHTML += '</div>'
 
           $('.main-container').html(employeeHTML);
         }); // end each
 
-        var modalHTML = '';
+
+
+        const profiles = $(".employee-card");
+        for (let i = 0; i < profiles.length; i++) {
+        profiles[i].onclick = function () {
 
         function modal(target, i) {
-          currentTarget = data.results.indexOf(target);
+          currentTarget = employee.indexOf(target);
           modalHTML += '<div class="modal-style">'
-//          employeeHTML += '<input id="slide-'+i+'-trigger" type="radio" name="slides">'
-  //        employeeHTML += '<section class="slide slide-'+i+'">'
+        //          employeeHTML += '<input id="slide-'+i+'-trigger" type="radio" name="slides">'
+        //        employeeHTML += '<section class="slide slide-'+i+'">'
           modalHTML += '<img src="'+target.picture.large+'" alt="'+target.name.first+'" class="modal-pic">'
           modalHTML += '<h2 >'+target.name.first+' '+target.name.last+'</h2>'
           modalHTML += '<p class="lowercase">'+target.email+'</p>'
@@ -63,6 +68,14 @@ let currentTarget = '';
 
           $('#employees'+i+'').html(modalHTML);
 
+          };
+        };
+      } //end for loop
+
+
+
+          $(".employee-card").click(function() {
+
 
           $("#next").click(function() {
               modal(currentTarget[i + 1], i + 1);
@@ -73,7 +86,7 @@ let currentTarget = '';
           });
 
 
-
+          });
             //      employeeHTML += '<i data-feather="chevron-left" id="prev"></i>'
             //      employeeHTML += '<i data-feather="chevron-right" id="next"></i>'
               //      employeeHTML += '</section>'
@@ -86,7 +99,8 @@ let currentTarget = '';
 
 
 
-  }
+
+
 
 
 
